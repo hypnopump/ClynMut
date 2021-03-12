cfb478b14b393818dc627cd41f665af28efe83d1
hypnopump <ericalcaide1@gmail.com>
2021-03-12 12:17:19 +0100

    adding initial infra. still package info lacking to go public

---
+++ clynmut/clynmut.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn.functional as F
+from einops import rearrange, repeat
+
+# data
+import sidechainnet as scn
+from sidechainnet.sequence.utils import VOCAB
+from sidechainnet.structure.build_info import NUM_COORDS_PER_RES
+
+# models
+from clynmut.utils import *
+from alphafold2_pytorch.utils import *
+
+
+class MutPredict(torch.nn.Module):
+    def __init__():
+        return
+
+    def forward(self, seqs, info=None, verbose=0):
+        """ Predicts the mutation effect in a protein. 
+            Inputs:
+            * seqs: list of strings (batch, seqs). Sequence in 1-letter AA code.
+            * info: any info required. 
+            * verbose: int. verbosity level (0-silent, 1-minimal, 2-full)
+        """
+        seq_embedds = self.embedd_batch(seqs) # (batch, embedd_size)
+
+        pred = None
+        return pred
+
---
+++ clynmut/utils.py
@@ -0,0 +1,68 @@
+import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+from einops import rearrange
+
+# data
+import sidechainnet as scn
+from sidechainnet.sequence.utils import VOCAB
+from sidechainnet.structure.build_info import NUM_COORDS_PER_RES
+
+# models
+from alphafold2_pytorch.utils import *
+
+
+# Constants / Config
+PADDING_TOKEN = "_"
+FEATURES = "esm" # one of ["esm", "msa", None]
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "CPU") 
+SAVE_DIR = ""
+
+
+# set emebdder model from esm if appropiate - Load ESM-1b model
+if FEATURES == "esm":
+    # from pytorch hub (almost 30gb)
+    EMBEDD_MODEL, ALPHABET = torch.hub.load("facebookresearch/esm", "esm1b_t33_650M_UR50S")
+    ##  alternatively do
+    # import esm # after installing esm
+    # model, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
+    BATCH_CONVERTER = alphabet.get_batch_converter()
+
+
+def embedd_batch(seqs):
+    """ Embedds a batch of sequences into mean of all AA embeddings.
+        Inputs:
+        * seqs: iterator of strs (batch, seqs)
+        Outputs: float tensor of size (batch, embedd_dims)
+    """
+    embedd_list = []
+    for seq in seqs:
+        seq_pure = seq.rstrip(PADDING_TOKEN)
+        embedd = get_esm_embedd(seq_pure)
+        # take mean
+        embedd_list.append( embedd.mean(dim=0, keepdim=True) )
+
+    return torch.cat(embedd_list, dim=0).to(DEVICE)
+
+
+def get_esm_embedd(seq):
+    """ Returns embeddings for all AAs of a protein sequence.
+        Inputs: 
+        * seq: str or tensor of ints (sidechainnet key-pair table) of length N
+        Outputs: float tensor of size (N, D)
+    """
+    # handles both sequences and int encodings
+    if isinstance(seq, torch.Tensor):
+        seq = "".join([VOCAB._int2char[x]for x in seq.cpu().numpy()])
+    # adjust input data and embedd
+    batch_labels, batch_strs, batch_tokens = batch_converter( [(0, seq)] )
+    with torch.no_grad():
+        results = embedd_model(batch_tokens, repr_layers=[33], return_contacts=False)
+    return results["representations"][33].to(DEVICE)
+
+
+
+
+
+
