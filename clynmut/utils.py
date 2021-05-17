e5e613170102437264d64d7216d4bb30cdaa9404
hypnopump <ericalcaide1@gmail.com>
2021-05-17 18:00:54 +0200

    update data

--- clynmut/utils.py
+++ clynmut/utils.py
@@ -9,9 +9,6 @@ import sidechainnet as scn
 from sidechainnet.utils.sequence import VOCAB
 from sidechainnet.structure.build_info import NUM_COORDS_PER_RES
 
-# models
-import alphafold2_pytorch.utils as af2utils
-
 
 # Constants / Config
 PADDING_TOKEN = "_"
@@ -24,9 +21,87 @@ SAVE_DIR = ""
 ### PRE-MODEL UTILS ###
 #######################
 
+def get_esm_embedd(seq, embedd_model, batch_converter, msa_data=None):
+    """ Returns the ESM embeddings for a protein. 
+        Inputs: 
+        * seq: (L,) tensor of ints (in sidechainnet int-char convention)
+        * embedd_model: ESM model (see train_end2end.py for an example)
+        * batch_converter: ESM batch converter (see train_end2end.py for an example)
+        * embedd_type: one of ["mean", "per_tok"]. 
+                       "per_tok" is recommended if working with sequences.
+        Outputs: tensor of (batch, n_seqs, L, embedd_dim)
+            * n_seqs: number of sequences in the MSA. 1 for ESM-1b
+            * embedd_dim: number of embedding dimensions. 
+                          768 for MSA_Transformer and 1280 for ESM-1b
+    """
+    str_seq = "".join([VOCAB._int2char[x] for x in seq.cpu().numpy()])
+    # use MSA transformer
+    if msa_data is not None: 
+        msa_batch_labels, msa_batch_strs, msa_batch_tokens = batch_converter(msa_data)
+        with torch.no_grad():
+            results = embedd_model(msa_batch_tokens.to(seq.device), repr_layers=[12], return_contacts=False)
+        # index 0 is for start token. so take from 1 one
+        token_reps = results["representations"][12][0, :,  1 : len(str_seq) + 1]
+        
+    # base ESM case
+    else: 
+        batch_labels, batch_strs, batch_tokens = batch_converter( [(0, str_seq)] )
+        with torch.no_grad():
+            results = embedd_model(batch_tokens.to(seq.device), repr_layers=[33], return_contacts=False)
+        # index 0 is for start token. so take from 1 one
+        token_reps = results["representations"][33][:, 1 : len(str_seq) + 1].unsqueeze(dim=1)
+    
+    return token_reps.mean(dim=-2) if embedd_type == "mean" else token_reps
 
-def embedd_seq_batch(seqs, msa_data=None, embedd_model=None, batch_converter=None):
-    """ Embedds a batch of sequences into mean of all AA embeddings.
+
+def get_t5_embedd(seq, tokenizer, encoder, msa_data=None, device=None, embedd_type="per_tok"):
+    """ Returns the ProtT5-XL-U50 embeddings for a protein.
+        Supports batched embedding as well as individual.
+        Inputs:
+        * seq: ( (b,) L,) tensor of ints (in sidechainnet int-char convention)
+        * tokenizer:  tokenizer model: T5Tokenizer
+        * model: encoder model: T5EncoderModel
+                 ex: from transformers import T5EncoderModel, T5Tokenizer
+                     model_name = "Rostlab/prot_t5_xl_uniref50"
+                     tokenizer = T5Tokenizer.from_pretrained(model_name, do_lower_case=False )
+                     model = T5EncoderModel.from_pretrained(model_name)
+                     # prepare model 
+                     model = model.to(device)
+                     model = model.eval()
+                     if torch.cuda.is_available():
+                         model = model.half()
+        Outputs: tensor of (batch, n_seqs, L, embedd_dim)
+            * n_seqs: number of sequences in the MSA. 1 for T5 models
+            * embedd_dim: number of embedding dimensions. 1024 for T5 models
+    """
+    # get params and prepare
+    device = seq.device if device is None else device
+    # format accordingly
+    embedd_inputs = []
+    seq = seq.unsqueeze(0) if len(seq.shape[0]) < 2 else seq
+    for ids in seq.cpu().tolist():
+        chars = ' '.join([id2aa[i] for i in ids])
+        chars = re.sub(r"[UZOB]", "X", chars)
+        out.append(chars)
+    
+    # embedd - https://huggingface.co/Rostlab/prot_t5_xl_uniref50
+    inputs_embedding = []
+    shift_left, shift_right = 0, -1
+    ids = tokenizer.batch_encode_plus(embedd_inputs, add_special_tokens=True,
+                                                     padding=True, 
+                                                     return_tensors="pt")
+    with torch.no_grad():
+        embedding = model(input_ids=torch.tensor(ids['input_ids']).to(device), 
+                          attention_mask=torch.tensor(ids["attention_mask"]).to(device),
+                          decoder_input_ids=None)
+    # return (batch, n_seqs, seq_len, embedd_dim)
+    token_reps = embedding.last_hidden_state[:, shift_left:shift_right].to(device).unsqueeze(1)
+
+    return token_reps.mean(dim=-2) if embedd_type == "mean" else token_reps
+
+
+def embedd_seq_batch_esm(seqs, msa_data=None, embedd_model=None, batch_converter=None):
+    """ Embedds a batch of sequences get_esm_embeddinto mean of all AA embeddings.
         Inputs:
         * seqs: iterator of strs (batch, seqs)
         Outputs: float tensor of size (batch, embedd_dims)
@@ -36,12 +111,11 @@ def embedd_seq_batch(seqs, msa_data=None, embedd_model=None, batch_converter=Non
         seq_pure = seq.rstrip(PADDING_TOKEN)
         msa_pure = [msa_data[i]] if msa_data is not None else None
         # take mean
-        embedd_list.append( af2utils.get_esm_embedd(seq_pure, msa_data=msa_pure,
-                                                              embedd_model=EMBEDD_MODEL, 
-                                                              batch_converter=BATCH_CONVERTER, 
-                                                              embedd_type="mean"))
-    return torch.stack(embedd_list, dim=0).to(DEVICE)
-
+        embedd_list.append( get_esm_embedd(seq_pure, msa_data=msa_pure,
+                                                     embedd_model=EMBEDD_MODEL, 
+                                                     batch_converter=BATCH_CONVERTER, 
+                                                     embedd_type="mean"))
+    return torch.stack(embedd_list, dim=-2).to(DEVICE)
 
 
 #######################
@@ -92,7 +166,7 @@ class Hier_Helper():
                     if node_list[j]["class"] in node["children"]:
                         children_idxs.append(j)
             # update nodes
-            node_list[i]["parent_idx"] = partent
+            node_list[i]["parent_idx"] = parent
             node_list[i]["children_idxs"] = children_idxs
 
         return node_list 
@@ -176,7 +250,7 @@ def hier_softmax(true_dict=None, pred_dict=None,
         * verbose: 0 for silent, 1 for full verbosity
     """
     loss = 0.
-    depth_adj = lambda x: 1/(1+x)if weight_mode == "div" else 1/2**x 
+    depth_adj = lambda x: 1/(1+x) if weight_mode == "div" else 1/2**x 
     # select the first level, then go down hierarchical tree
     level = 0
     next_key = True
